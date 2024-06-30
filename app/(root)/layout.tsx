caa7958807a3c72a7158ebc3f7f62ac28738ff86
Chu <ttheko101@gmail.com>
2024-06-30 23:06:11 +0200

    Added metadata

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
+
+export const metadata: Metadata = {
+  title: "SNAPDART",
+  description: "Right On The Money.",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
