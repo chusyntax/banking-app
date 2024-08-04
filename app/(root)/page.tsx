ff253b17e19eda33f95d7856e6c1945dcd9437bf
Chu <ttheko101@gmail.com>
2024-08-04 20:29:26 +0200

    Added Chart and welcome banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <section className="home">Home</section>;
+  const loggedIn = { firstName: "Chu" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome,"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.36}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
