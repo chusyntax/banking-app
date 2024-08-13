c3495bdc69f5938343e7ce625cd50243a01084be
Chu <ttheko101@gmail.com>
2024-08-13 21:35:38 +0200

    Changed stars

--- components/ui/BankCard.tsx
+++ components/ui/BankCard.tsx
@@ -26,7 +26,7 @@ const BankCard = ({
               <h2 className="text-12 font-semibold text-white">** / **</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              **** **** **** <span className="text-16">${account.mask}</span>
+              **** <span className="text-16">${account.mask}</span>
             </p>
           </article>
         </div>
